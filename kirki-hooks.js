c26002b863ac282540efe8954444b8053853f8b0
Ignacio Cruz <igmoweb@gmail.com>
2015-09-07 19:12:42 +0200

    .

--- kirki-hooks.js
+++ kirki-hooks.js
@@ -1,43 +1,170 @@
-wp.customize.controlConstructor['kirki-repeater'] = wp.customize.Control.extend({
+wp.customize.controlConstructor['repeater'] = wp.customize.Control.extend({
     ready: function() {
-        var control = this;
-        var api = wp.customize;
+        var control = this,
 
-        // I just got this from customize-control.js Don't ask for the moment
-        var settings = control.container.find('[data-customize-setting-link]');
-        var radios;
-        settings.each( function() {
-            var node = jQuery( this ),
-                name;
+        // The current value set in Control Class (set in Kirki_Customize_Repeater_Control::to_json() function)
+            settingValue = this.params.value;
 
-            if ( node.is( ':radio' ) ) {
-                name = node.prop( 'name' );
-                if ( radios[ name ] ) {
-                    return;
+        // The hidden field that keeps the data saved (though we never update it)
+        this.settingField = this.container.find('[data-customize-setting-link]').first(),
+
+            // Set the field value for the first time, we'll fill it up later
+            this.setValue([], false);
+
+        // The DIV that holds all the rows
+        this.repeaterFieldsContainer = control.container.find('.repeater-fields').first();
+
+        // Set number of rows to 0
+        this.currentIndex = 0;
+
+        control.container.on('click', 'button.repeater-add', function (e) {
+            e.preventDefault();
+            control.addRow();
+        });
+
+        /**
+         * Function that loads the Mustache template
+         */
+        this.repeaterTemplate = _.memoize(function () {
+            var compiled,
+            /*
+             * Underscore's default ERB-style templates are incompatible with PHP
+             * when asp_tags is enabled, so WordPress uses Mustache-inspired templating syntax.
+             *
+             * @see trac ticket #22344.
+             */
+                options = {
+                    evaluate: /<#([\s\S]+?)#>/g,
+                    interpolate: /\{\{\{([\s\S]+?)\}\}\}/g,
+                    escape: /\{\{([^\}]+?)\}\}(?!\})/g,
+                    variable: 'data'
+                };
+
+            return function (data) {
+                compiled = _.template(control.container.find('.customize-control-repeater-content').first().html(), null, options);
+                return compiled(data);
+            };
+        });
+
+        // When we load the control, the fields have not been filled up
+        // This is the first time that we create all the rows
+        if (settingValue.length) {
+            for (var i = 0; i < settingValue.length; i++) {
+                control.addRow(settingValue[i]);
+            }
+        }
+
+        this.container
+            .on( 'keyup change', '.repeater-fields input', function( e ) {
+                control.updateField.call( control, e );
+            });
+    },
+
+
+
+    /**
+     * Get the current value of the setting
+     *
+     * @return Object
+     */
+    getValue: function() {
+        // The setting is saved in JSON
+        return JSON.parse( decodeURI( this.setting.get() ) );
+    },
+
+    /**
+     * Set a new value for the setting
+     *
+     * @param newValue Object
+     * @param refresh If we want to refresh the previewer or not
+     */
+    setValue: function( newValue, refresh ) {
+        this.setting.set( encodeURI( JSON.stringify( newValue ) ) );
+
+        if ( refresh ) {
+            // Trigger the change event on the hidden field so
+            // previewer refresh the website on Customizer
+            this.settingField.trigger('change');
+        }
+    },
+
+    /**
+     * Add a new row to repeater settings based on the structure.
+     *
+     * @param data (Optional) Object of field => value pairs (undefined if you want to get the default values)
+     */
+    addRow: function( data ) {
+        var control = this,
+        i,
+
+        // The template for the new row (defined on Kirki_Customize_Repeater_Control::render_content() )
+        template = control.repeaterTemplate(),
+
+        // Get the current setting value
+        settingValue = this.getValue(),
+
+        // Saves the new setting data
+        newRowSetting = {},
+
+        // Data to pass to the template
+        templateData;
+
+        if ( template ) {
+
+            // The control structure is going to define the new fields
+            templateData = control.params.fields;
+
+            // But if we have passed data, we'll use the data values instead
+            if ( data ) {
+                for ( i in data ) {
+                    if ( data.hasOwnProperty( i ) && templateData.hasOwnProperty( i ) ) {
+                        templateData[i].default = data[i];
+                    }
                 }
-
-                radios[ name ] = true;
-                node = nodes.filter( '[name="' + name + '"]' );
             }
 
-            api( node.data( 'customizeSettingLink' ), function( setting ) {
-                var element = new api.Element( node );
-                control.elements.push( element );
-                element.sync( setting );
-                element.set( setting() );
-            });
-        });
+            templateData['index'] = this.currentIndex;
+
+            // Append the template content
+            template = template( templateData );
+            control.repeaterFieldsContainer.append( template );
 
 
-        control.container.on( 'click', 'button.repeater-add-new', function( e ) {
-            e.preventDefault();
-            control.settings[ 1 ] = api( 'fefefefe' );
-            console.log(control);
-        });
+            for ( i in templateData ) {
+                if ( templateData.hasOwnProperty( i ) ) {
+                    newRowSetting[ i ] = templateData[i].default;
+                }
+            }
 
+            settingValue.push( newRowSetting );
+            this.setValue( settingValue, true );
 
+            this.currentIndex++;
+
+        }
+    },
+    deleteRow: function() {
+
+    },
+    updateField: function( e ) {
+        var element = jQuery( e.target ),
+        control = this,
+        currentSettings = this.getValue();
+
+        // Gather data about the field row + ID
+        var row = element.data( 'row' );
+        var fieldId = element.data( 'field' );
+
+        if ( typeof currentSettings[row][fieldId] == undefined )
+            return false;
+
+        // Update the settings
+        currentSettings[row][fieldId] = element.val();
+        control.setValue( currentSettings, true );
     }
 });
+
+
 /**wp.customize.controlConstructor['kirki-repeater'] = wp.customize.Control.extend({
     ready: function () {
         var control = this;
--- kirki-hooks.php
+++ kirki-hooks.php
@@ -1,97 +1,177 @@
 <?php
 
-add_action( 'customize_register', 'kirki_hooks_customize_register' );
+add_action( 'customize_register', 'kirki_hooks_customize_register', 500 );
 function kirki_hooks_customize_register( $wp_customize ) {
 
-	class Kirki_Customize_Repeater_Button_Setting extends WP_Customize_Setting {
+	class Kirki_Customize_Repeater_Setting extends WP_Customize_Setting {
+		public function __construct( $manager, $id, $args = array() ) {
+			parent::__construct( $manager, $id, $args );
+		}
 
+		public function value() {
+			$value = parent::value();
+			if ( ! is_array( $value ) )
+				$value = array();
+
+			return $value;
+		}
+
+		protected function update( $value ) {
+			$value = json_decode( urldecode( $value ) );
+
+			if ( empty( $value ) || ! is_array( $value ) )
+				$value = array();
+
+			// Customizer will send up an array of objects,
+			// we are going to cast those to arrays
+			foreach ( $value as $row => $object ) {
+				$value[ $row ] = (array)$object;
+			}
+
+			switch( $this->type ) {
+				case 'theme_mod' :
+					return $this->_update_theme_mod( $value );
+
+				case 'option' :
+					return $this->_update_option( $value );
+
+				default :
+
+					/**
+					 * Fires when the {@see WP_Customize_Setting::update()} method is called for settings
+					 * not handled as theme_mods or options.
+					 *
+					 * The dynamic portion of the hook name, `$this->type`, refers to the type of setting.
+					 *
+					 * @since 3.4.0
+					 *
+					 * @param mixed                $value Value of the setting.
+					 * @param WP_Customize_Setting $this  WP_Customize_Setting instance.
+					 */
+					return do_action( 'customize_update_' . $this->type, $value, $this );
+			}
+		}
 	}
 
 	class Kirki_Customize_Repeater_Control extends WP_Customize_Control {
-		public $type = 'kirki-repeater';
+		public $type = 'repeater';
 		public $fields = array();
 		public $button_label = "";
 
 		public function __construct( $manager, $id, $args = array() ) {
 			parent::__construct( $manager, $id, $args );
+
+			if ( empty( $this->button_label ) )
+				$this->button_label = 'Add new row';
 		}
 
 		public function to_json() {
 			parent::to_json();
 
-			$this->json['id'] = $this->id;
 			$this->json['fields'] = $this->fields;
-			$this->json['buttonSetting'] = $this->button_setting_id;
-			$this->json['buttonLabel'] = $this->button_label;
+			$this->json['value'] = $this->value();
 		}
 
-		public function set_button_setting_id( $id ) {
-			$this->button_setting_id = $id;
-		}
+
 
 		public function enqueue() {
 			wp_enqueue_script( 'kirki-repeater-customize-controls', plugin_dir_url( __FILE__ ) . 'kirki-hooks.js', array( 'jquery', 'customize-base' ), '123', true );
+			wp_enqueue_style( 'kirki-repeater-customize-controls', plugin_dir_url( __FILE__ ) . 'kirki-hooks.css' );
 		}
 
 
 		public function render_content() {
-			foreach ( $this->settings as $setting_key => $setting ) {
+			$value = json_encode( $this->value() );
+			$id = $this->id;
+			?>
+			<label>
+				<?php if ( ! empty( $this->label ) ) : ?>
+					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+				<?php endif;
+				if ( ! empty( $this->description ) ) : ?>
+					<span class="description customize-control-description"><?php echo $this->description; ?></span>
+				<?php endif; ?>
+				<input type="hidden" <?php $this->input_attrs(); ?> value="" <?php echo $this->get_link(); ?> />
+			</label>
 
-				?>
-				<label>
-					<?php if ( ! empty( $this->label ) ) : ?>
-						<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-					<?php endif;
-					if ( ! empty( $this->description ) ) : ?>
-						<span class="description customize-control-description"><?php echo $this->description; ?></span>
-					<?php endif; ?>
-					<input type="text" <?php $this->input_attrs(); ?> value="<?php echo esc_attr( $this->value( $setting_key ) ); ?>" <?php echo $this->get_link( $setting_key ); ?> />
-				</label>
-				<?php
-			}
+			<div class="repeater-fields"></div>
+
+			<button class="button-secondary repeater-add"><?php echo esc_html( $this->button_label ); ?></button>
+			<?php
+
+			$this->repeater_js_template();
 
 		}
 
-		public function content_template() {
+		public function repeater_js_template() {
 			?>
+			<script type="text/html" class="customize-control-repeater-content">
 				<#
-					console.log(data);
-					if ( ! data )
-						return;
 
-					var api = wp.customize;
-					for ( var i = 0; i < data.settings.length; i++ ) {
-						var setting_key = data.settings[i];
-						var setting = api.settings.settings[ setting_key ];
+					var field;
+					var index = data['index'];
 
-						if ( setting_key === data.buttonSetting ) {
-							// Render the button
+					#>
+					<div class="repeater-row" data-row="{{{ index }}}">
+					<#
+
+					for ( i in data ) {
+						if ( ! data.hasOwnProperty( i ) )
+							continue;
+
+						field = data[i];
+
+						if ( field.type === 'text' ) {
 							#>
-							<input type="text" value="{{{ setting.value }}}" data-customize-setting-link="{{{ setting_key }}}" />
-							<button class="repeater-add-new">{{ data.buttonLabel }}</button>
+								<label>
+									<# if ( field.label ) { #>
+										<span class="customize-control-title">{{ field.label }}</span>
+									<# } #>
+									<# if ( field.description ) { #>
+										<span class="description customize-control-description">{{ field.description }}</span>
+									<# } #>
+									<input type="text" name="" value="{{{ field.default }}}" data-field="{{{ field.id }}}" data-row="{{{ index }}}">
+								</label>
 							<#
 						}
-						else {
-							// Render the field
+						else if ( field.type === 'checkbox' ) {
+								console.log(field.default);
+							#>
+							<label>
+								<# if ( field.label ) { #>
+									<span class="customize-control-title">{{ field.label }}</span>
+								<# } #>
+								<# if ( field.description ) { #>
+									<span class="description customize-control-description">{{ field.description }}</span>
+								<# } #>
 
+								<# if ( field.default ) { #>
+									<input type="checkbox" name="" data-field="{{{ field.id }}}" data-row="{{{ index }}}" checked="checked" />
+								<# } else { #>
+									<input type="checkbox" name="" data-field="{{{ field.id }}}" data-row="{{{ index }}}" />
+								<# } #>
+
+							</label>
+							<#
 						}
-						console.log(setting_key);
-						console.log(wp.customize.settings);
-
 					}
 
+					#>
+					</div>
+					<#
 				#>
 
+			</script>
 			<?php
 		}
 
+
 	}
-		// All this will be passed to a new Kirki class/function
+
+	// All this will be passed to a new Kirki class/function
 	$repeater_id = 'my-repeater';
 	$section_id = 'repeater_test';
 	$button_label = "Add new Row";
-
-	$theme_mod = false;
 
 	$structure = array(
 		'subsetting_1' => array(
@@ -101,21 +181,24 @@ function kirki_hooks_customize_register( $wp_customize ) {
 		),
 		'subsetting_2' => array(
 			'type' => 'text',
-			'label' => 'Setting B'
+			'label' => 'Setting B',
+			'default' => ''
 		)
 	);
-
-
-
+	// End of data. Everything will be done by Kirki from now on
 
 
 
 
 	foreach ( $structure as $key => $value ) {
+		if ( ! isset( $value['default'] ) )
+			$structure[ $key ]['default'] = '';
+
+		if ( ! isset( $value['label'] ) )
+			$structure[ $key ]['default'] = '';
 		$structure[ $key ]['id'] = $key;
 	}
 
-	$wp_customize->register_control_type( 'Kirki_Customize_Repeater_Control' );
 
 	$wp_customize->add_section( $section_id, array(
 		'title'          => __( 'Repeater Test', 'themename' ),
@@ -123,25 +206,27 @@ function kirki_hooks_customize_register( $wp_customize ) {
 		'transport'     => 'refresh'
 	) );
 
-	if ( ! is_array( $theme_mod ) || empty( $theme_mod ) )
-		$theme_mod = array();
 
-	$total_rows = count( $theme_mod );
-	$repeater_settings = array();
-
-
-	$button_setting_id = $repeater_id . '-button';
-	$repeater_settings[] = $button_setting_id ;
-	$wp_customize->add_setting( new Kirki_Customize_Repeater_Button_Setting( $wp_customize, $button_setting_id, array(
-		'default' => $total_rows // We pass the number of current rows to the button setting. It's the one that will control the number of rows
+	$wp_customize->add_setting( new Kirki_Customize_Repeater_Setting( $wp_customize, $repeater_id, array(
+		'type' => 'theme_mod',
+		'capability' => 'manage_options',
+		'theme_supports' => '',
+		'transport' => 'refresh',
+		'sanitize_callback' => '',
+		'sanitize_js_callback' => ''
 	) ) );
 
 	$control = new Kirki_Customize_Repeater_Control( $wp_customize, $repeater_id, array(
 		'section' => $section_id,
-		'settings' => $repeater_settings,
 		'fields' => $structure,
-		'button_label' => $button_label
+		'button_label' => $button_label,
+		'label' => '',
+		'description' => '',
+		'priority' => ''
 	) );
-	$control->set_button_setting_id( $button_setting_id );
+
 	$wp_customize->add_control( $control );
+
+
+
 }
